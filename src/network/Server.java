a0051294548f4e2d093003a32d3c2e13f7b95e35
Harsha Gotur <hgotur@umich.edu>
2013-11-27 16:05:26 -0500

    changed server

--- src/network/Server.java
+++ src/network/Server.java
@@ -4,20 +4,26 @@ import static java.lang.System.out;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
+
 import Game.*;
 
 public class Server implements Runnable {
-  private ClientServerSocket server1;
-  private ClientServerSocket server2;
-  private ClientServerSocket server3;
-  private ClientServerSocket server4;
+//  private ClientServerSocket server1;
+//  private ClientServerSocket server2;
+//  private ClientServerSocket server3;
+//  private ClientServerSocket server4;
+  
+  private ArrayList<ClientServerSocket> server;
   private GameController game;
+  private ServerSender serverSender;
 
   public Server(GameController theGame) {
-      server1 = new ClientServerSocket("127.0.0.1", 45001);
-      server2 = new ClientServerSocket("127.0.0.1", 45001);
-      server3 = new ClientServerSocket("127.0.0.1", 45001);
-      server4 = new ClientServerSocket("127.0.0.1", 45001);
+//      server1 = new ClientServerSocket("127.0.0.1", 45001);
+//      server2 = new ClientServerSocket("127.0.0.1", 45001);
+//      server3 = new ClientServerSocket("127.0.0.1", 45001);
+//      server4 = new ClientServerSocket("127.0.0.1", 45001);
+	  server = new ArrayList<ClientServerSocket>(0);
       game = theGame;
   }
 
@@ -27,27 +33,27 @@ public class Server implements Runnable {
     // while (game not started) {
         // if numPlayers < 4 accept connection
       ServerSocket serverSocket = new ServerSocket(45001);
-      out.println("Waiting for client1 to connect...");
-      server1.startServer(serverSocket);
-      Thread serverReceiver1 = new Thread(new Receiver(server1));
-      Thread serverSender = new Thread(new ServerSender(server1, server2, server3, server4));
-      serverReceiver1.start();
-      serverSender.start();
       
-      out.println("Waiting for client2 to connect...");
-      server2.startServer(serverSocket);
-      Thread serverReceiver2 = new Thread(new Receiver(server2));
-      serverReceiver2.start();
+      int i = 0;
+      while (true) {
+    	  // break out when game starts
+    	  if (server.size() < 4) {
+    		  ClientServerSocket clientServerSocket = new ClientServerSocket("127.0.0.1", 45001);
+        	  
+        	  out.println("Waiting for client1 to connect...");
+        	  clientServerSocket.startServer(serverSocket);
+        	  
+        	  
+        	  
+        	  Thread serverReceiver = new Thread(new ServerReceiver(clientServerSocket));
+              //Thread serverSender = new Thread(new ServerSender(server1, server2, server3, server4));
+              serverReceiver.start();  
+    	  }
+      }
       
-      out.println("Waiting for client3 to connect...");
-      server3.startServer(serverSocket);
-      Thread serverReceiver3 = new Thread(new Receiver(server3));
-      serverReceiver3.start();
-      
-      out.println("Waiting for client4 to connect...");
-      server4.startServer(serverSocket);
-      Thread serverReceiver4 = new Thread(new Receiver(server4));
-      serverReceiver4.start();
+      serverSender = new ServerSender(server);
+//      Thread serverSenderThread = new Thread(serverSender);
+//      serverSenderThread.start();
     }
     catch (IOException ioe) {
       out.println("ERROR: Caught exception starting server");
