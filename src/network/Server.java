7374442958c6c6a458a683df8407eb129581b1b7
Pratik Kabra <pratik@kabra.com>
2013-11-27 15:19:14 -0500

    Make changes to Server for ServerSender

--- src/network/Server.java
+++ src/network/Server.java
@@ -1,4 +1,5 @@
 package network;
+
 import static java.lang.System.out;
 
 import java.io.IOException;
@@ -29,30 +30,24 @@ public class Server implements Runnable {
       out.println("Waiting for client1 to connect...");
       server1.startServer(serverSocket);
       Thread serverReceiver1 = new Thread(new Receiver(server1));
-      Thread serverSender1 = new Thread(new Sender(server1));
+      Thread serverSender = new Thread(new ServerSender(server1, server2, server3, server4));
       serverReceiver1.start();
-      serverSender1.start();
+      serverSender.start();
       
       out.println("Waiting for client2 to connect...");
       server2.startServer(serverSocket);
       Thread serverReceiver2 = new Thread(new Receiver(server2));
-      Thread serverSender2 = new Thread(new Sender(server2));
       serverReceiver2.start();
-      serverSender2.start();
       
       out.println("Waiting for client3 to connect...");
       server3.startServer(serverSocket);
       Thread serverReceiver3 = new Thread(new Receiver(server3));
-      Thread serverSender3 = new Thread(new Sender(server3));
       serverReceiver3.start();
-      serverSender3.start();
       
       out.println("Waiting for client4 to connect...");
       server4.startServer(serverSocket);
       Thread serverReceiver4 = new Thread(new Receiver(server4));
-      Thread serverSender4 = new Thread(new Sender(server4));
       serverReceiver4.start();
-      serverSender4.start();
     }
     catch (IOException ioe) {
       out.println("ERROR: Caught exception starting server");
