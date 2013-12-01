5bcf3bdf4ed891d636eb1105edeaa064c7106f36
Pratik Kabra <pratik@kabra.com>
2013-12-01 18:34:49 -0500

    Server now responds to full or started games

--- src/network/ServerAcceptThread.java
+++ src/network/ServerAcceptThread.java
@@ -16,19 +16,20 @@ public class ServerAcceptThread implements Runnable {
   
   public void run() {
     System.out.println("Starting accepting connections.");
-    while (server.sockets.size() < 4 && !server.hasGameStarted()) {
+    while (true) {
   	  // break out when game starts
-  	  if (server.sockets.size() < 4) {
-  		  ClientServerSocket clientServerSocket = new ClientServerSocket("127.0.0.1", GameController.PORT);
-    	  if(!server.hasGameStarted()) {
-    	    clientServerSocket.startServer(serverSocket);
-    	    server.sockets.add(clientServerSocket);
-    	    Thread serverReceiver = new Thread(new ServerReceiver(server, clientServerSocket, server.sockets.size()));
-          serverReceiver.start();
-    	  }
-    	  else {
-    	    break;
-    	  }
+		  ClientServerSocket clientServerSocket = new ClientServerSocket("127.0.0.1", GameController.PORT);
+		  clientServerSocket.startServer(serverSocket);
+  	  if(!server.hasGameStarted() && server.sockets.size() < 4) {
+  	    server.sockets.add(clientServerSocket);
+  	    Thread serverReceiver = new Thread(new ServerReceiver(server, clientServerSocket, server.sockets.size()));
+        serverReceiver.start();
+  	  }
+  	  else if(server.hasGameStarted()){
+  	    clientServerSocket.sendString("GAS");
+  	  }
+  	  else {
+  	    clientServerSocket.sendString("GF");
   	  }
     }
   }
