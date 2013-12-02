6e88aa52df06ffea05cc6aad79eb503611cdcede
Harsha Gotur <hgotur@umich.edu>
2013-12-02 01:09:07 -0500

    Fixed gameController, gameClientEngine code

--- src/Game/GameClientEngine.java
+++ src/Game/GameClientEngine.java
@@ -1,145 +1,175 @@
 package Game;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import view.*;
-import network.Client;
 
 public class GameClientEngine {
-	private GameController game;
+  private GameController game;
 
-	
-	private LevelScreen levelScreen;
-	private ArrayList<String> levelCommands;
-	private ArrayList<String> levelButtons;
-	private ArrayList<String> playerButtons;
-	private ArrayList<String> levelTextArray;
-	private ArrayList<Integer> levelTimer;
-	private ArrayList<Integer> levelShipDamage;
-	int curLevel;
-	int curCommandIndex;
-	
-	public GameClientEngine(GameController theGame) {
-	  game = theGame;
-	
-	  
-		  levelCommands = new ArrayList<String>(0);
-		  levelButtons = new ArrayList<String>(0);
-		  playerButtons = new ArrayList<String>(0);
-		  levelTextArray = new ArrayList<String>(0);
-		  levelTimer = new ArrayList<Integer>(0);
-		  levelShipDamage = new ArrayList<Integer>(0);
-	    InputStream levelCommandsFile = getClass().getResourceAsStream("levelCommands.txt");
-	    InputStream levelButtonsFile = getClass().getResourceAsStream("levelButtons.txt");
-	    InputStream levelTextFile = getClass().getResourceAsStream("levelText.txt");
-	    InputStream clientLevelSettingsFile = getClass().getResourceAsStream("levelSettings.txt");
-	    Scanner input = new Scanner(levelCommandsFile);
-	    while(input.hasNext()) {
-	      levelCommands.add(input.nextLine());
-	    }
-	    input = new Scanner(levelButtonsFile);
-	    while(input.hasNext()) {
-	      levelButtons.add(input.nextLine());
-	    }
-	    input = new Scanner(levelTextFile);
-	    while (input.hasNext()) {
-	    	levelTextArray.add(input.nextLine());
-	    }
-	    input = new Scanner(clientLevelSettingsFile);
-	    while (input.hasNext()) {
-	    	levelTimer.add(input.nextInt());
-	    	levelShipDamage.add(input.nextInt());
-	    	input.nextInt();
-	    	input.nextInt();
-	    }
-	    for(String text : levelTextArray){
-	    	System.out.println(text);
-	    }
-	}
-	
-//	public void waitingStatus(String [] players, boolean [] isReady) {
-//		game.waitingRoom.waitingStatus(players, isReady);
-//	}
-	
-	public void addPlayersToWaitingRoom(ArrayList<Player> players) {
-	  System.out.println("Adding players to room");
-	  game.waitingRoom.clearWaitingRoom();
-	  int i = 0;
-	  for(Player player : players) {
-	    game.waitingRoom.addPlayer(player.name, player.status , i);
-	    i++;
-	  }
-	}
-	
-	public void letsGetStarted() {
-		game.waitingRoom.setVisible(false);
-	}
-	
-	public void getNewLevelSetup(int level) {
-		curLevel = level;
-		if (levelScreen != null) {
-			levelScreen.timer.stop();
-			levelScreen.setVisible(false);
-		}
-		game.gameStartScreen = new GameStartScreen(level, levelTextArray.get(level - 1));
-	    
-	}
-	
-	public boolean isUser(String username) {
-		if (game.player.name.equals(username)) {
-			return true;
-		}
-		return false;
-	}
-	
-	public void addButtonsToLevel(String username, ArrayList<Integer> buttons) {
-		if (!isUser(username)) return;
-		levelScreen = new LevelScreen(this, curLevel, levelTimer.get(curLevel - 1), levelShipDamage.get(curLevel - 1));
-		ArrayList<String> buttonText = new ArrayList<String>(0);
-		for (int i : buttons) {
-			buttonText.add(levelButtons.get(i));
-		}
-		
-		levelScreen.addButtons(buttonText, buttons);
-	}
-	
-	public void levelStart() {
-		game.gameStartScreen.setVisible(false);
-		levelScreen.setVisible(true);
-	}
-	
-	public void readCommand(String username, int command) {
-		if (!isUser(username)) return;
-		levelScreen.newCommand(levelCommands.get(command));
-		curCommandIndex = command;
-	}
-	
-	public void sendButtonInput(int buttonIndex) {
-		game.client.sendButtonInput(buttonIndex);		
-	}
-	
-	public void sendCommandFailed() {
-		game.client.sendCommandFailed(curCommandIndex);
-	}
-	
-	public void setShipDamage(int damage) {
-	  levelScreen.setShipDamage(damage);
-	}
-	
-	public void endGame() {
-		levelScreen.setVisible(false);
-		levelScreen.timer.stop();
-		game.gameOverScreen = new GameOverScreen();
-	}
-	
-	public void gameWon() {
-		levelScreen.setVisible(false);
-		levelScreen.timer.stop();
-		game.gameWonScreen = new GameWonScreen();
-	}
+  private ArrayList<String> levelCommands;
+  private ArrayList<String> levelButtons;
+  private ArrayList<String> levelTextArray;
+  private ArrayList<Integer> levelTimer;
+  private ArrayList<Integer> levelShipDamage;
+  
+  private int curLevel;
+  private int curCommandIndex;
+
+  public GameClientEngine(GameController theGame) {
+    game = theGame;
+
+    Scanner input;
+    
+    // populate level commands array
+    levelCommands = new ArrayList<String>(0);
+    InputStream levelCommandsFile = getClass().getResourceAsStream("levelCommands.txt");
+    input = new Scanner(levelCommandsFile);
+    while (input.hasNext()) {
+      levelCommands.add(input.nextLine());
+    }
+
+    // populate level buttons array
+    levelButtons = new ArrayList<String>(0);
+    InputStream levelButtonsFile = getClass().getResourceAsStream("levelButtons.txt");
+    input = new Scanner(levelButtonsFile);
+    while (input.hasNext()) {
+      levelButtons.add(input.nextLine());
+    }
+    
+    // populate level text array
+    levelTextArray = new ArrayList<String>(0);
+    InputStream levelTextFile = getClass().getResourceAsStream("levelText.txt");
+    input = new Scanner(levelTextFile);
+    while (input.hasNext()) {
+      levelTextArray.add(input.nextLine());
+    }
+    
+    // populate level timer and level ship damage arrays
+    levelTimer = new ArrayList<Integer>(0);
+    levelShipDamage = new ArrayList<Integer>(0);    
+    InputStream clientLevelSettingsFile = getClass().getResourceAsStream("levelSettings.txt");
+    input = new Scanner(clientLevelSettingsFile);
+    while (input.hasNext()) {
+      levelTimer.add(input.nextInt());
+      levelShipDamage.add(input.nextInt());
+      input.nextInt(); input.nextInt(); // not used by the client
+    }
+  }
+
+  /*
+   * Display list of players in the waiting room along with their status.
+   */
+  public void addPlayersToWaitingRoom(ArrayList<Player> players) {
+    game.waitingRoom.clearWaitingRoom();
+    int i = 0;
+    for (Player player : players) {
+      game.waitingRoom.addPlayer(player.name, player.status, i);
+      i++;
+    }
+  }
+
+  /*
+   * Leave the waiting screen.
+   */
+  public void letsGetStarted() {
+    game.waitingRoom.setVisible(false);
+  }
+
+  /*
+   * Load the next level screen. Initialize level data. Stop timer on old level.
+   */
+  public void getNewLevelSetup(int level) {
+    curLevel = level;
+    if (game.levelScreen != null) {
+      game.levelScreen.timer.stop();
+      game.levelScreen.setVisible(false);
+    }
+    
+    game.gameStartScreen = new GameStartScreen(level, levelTextArray.get(level - 1));
+  }
+
+  /*
+   * Returns if this client is the one that a command refers to.
+   */
+  public boolean isUser(String username) {
+    if (game.player.name.equals(username)) {
+      return true;
+    }
+    return false;
+  }
+
+  /*
+   * Add buttons to the level screen.
+   */
+  public void addButtonsToLevel(String username, ArrayList<Integer> buttons) {
+    if (!isUser(username)) return;
+    game.levelScreen = new LevelScreen(this, curLevel, levelTimer.get(curLevel - 1),
+        levelShipDamage.get(curLevel - 1));
+    ArrayList<String> buttonText = new ArrayList<String>(0);
+    for (int i : buttons) {
+      buttonText.add(levelButtons.get(i));
+    }
+
+    game.levelScreen.addButtons(buttonText, buttons);
+  }
+
+  /*
+   * Hide the level start screen and show the level screen.
+   */
+  public void levelStart() {
+    game.gameStartScreen.setVisible(false);
+    game.levelScreen.setVisible(true);
+  }
+
+  /*
+   * Read the next instruction passed to the client by the server.
+   * Calls levelScreen.newCommand to display on level screen.
+   */
+  public void readCommand(String username, int command) {
+    if (!isUser(username)) return;
+    game.levelScreen.newCommand(levelCommands.get(command));
+    curCommandIndex = command;
+  }
+
+  /*
+   * Sends client button input to server.
+   */
+  public void sendButtonInput(int buttonIndex) {
+    game.client.sendButtonInput(buttonIndex);
+  }
+
+  /*
+   * Sends that instruction was not completed to the server.
+   */
+  public void sendCommandFailed() {
+    game.client.sendCommandFailed(curCommandIndex);
+  }
+
+  /*
+   * Displays the new ship damage on the level screen.
+   */
+  public void setShipDamage(int damage) {
+    game.levelScreen.setShipDamage(damage);
+  }
+
+  /*
+   * Displays the Game Over (players lost) screen
+   */
+  public void endGame() {
+    game.levelScreen.setVisible(false);
+    game.levelScreen.timer.stop();
+    game.gameOverScreen = new GameOverScreen();
+  }
+
+  /*
+   * Displays the Game Won (players won) screen
+   */
+  public void gameWon() {
+    game.levelScreen.setVisible(false);
+    game.levelScreen.timer.stop();
+    game.gameWonScreen = new GameWonScreen();
+  }
 }
